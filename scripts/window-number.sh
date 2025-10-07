15724b98c3fe16cb80233b372e290271f259fc41
Dustin Schultz <dustindschultz@gmail.com>
2025-10-06 23:09:07 -0400

    updated tab number style

--- scripts/window-number.sh
+++ scripts/window-number.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# 󰎤 󰎧 󰎪 󰎭 󰎱 󰎳 󰎶 󰎹 󰎼 󰎿
 
 case $1 in
-  1) echo "󰎤" ;;
-  2) echo "󰎧" ;;
-  3) echo "󰎪" ;;
-  4) echo "󰎭" ;;
-  5) echo "󰎱" ;;
-  6) echo "󰎳" ;;
-  7) echo "󰎶" ;;
-  8) echo "󰎹" ;;
-  9) echo "󰎼" ;;
-  *) echo "󰎿" ;;
+  1) echo "󰲠" ;;
+  2) echo "󰲢" ;;
+  3) echo "󰲤" ;;
+  4) echo "󰲦" ;;
+  5) echo "󰲨" ;;
+  6) echo "󰲪" ;;
+  7) echo "󰲬" ;;
+  8) echo "󰲮" ;;
+  9) echo "󰲰" ;;
+  10) echo "󰲞" ;;
 esac
